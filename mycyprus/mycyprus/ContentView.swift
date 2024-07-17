cb2838b39382106c43993b6620cb7e2fb74f0cd2
Sergey Lavrov <lavrov-sergey@yandex.ru>
2024-07-17 19:44:55 +0300

    Создан пустой проект SwiftUI

---
+++ mycyprus/mycyprus/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  mycyprus
+//
+//  Created by Sergey on 17.07.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ mycyprus/mycyprus/mycyprusApp.swift
@@ -0,0 +1,17 @@
+//
+//  mycyprusApp.swift
+//  mycyprus
+//
+//  Created by Sergey on 17.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct mycyprusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
